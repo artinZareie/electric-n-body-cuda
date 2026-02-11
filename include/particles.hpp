3b11744a3e829cd9791e975a1636af3bd69650b5
Artin Zarei <artin.zareie@yahoo.com>
2026-02-11 16:47:21 +0330

    Fixed CPU/GPU compatibality

--- include/particles.hpp
+++ include/particles.hpp
@@ -2,6 +2,7 @@
 
 #include "particle.hpp"
 #include <cstddef>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -10,20 +11,30 @@ struct ParticlesView
     float *__restrict__ x, *__restrict__ y, *__restrict__ z;
     float *__restrict__ vx, *__restrict__ vy, *__restrict__ vz;
     float *__restrict__ m, *__restrict__ q;
-
-  public:
-    void cpu_free();
 };
 
 class Particles
 {
   private:
-    std::size_t m_size = 0;
-    float *m_x, *m_y, *m_z;
-    float *m_vx, *m_vy, *m_vz;
-    float *m_m, *m_q;
+    std::vector<float> m_x, m_y, m_z;
+    std::vector<float> m_vx, m_vy, m_vz;
+    std::vector<float> m_m, m_q;
 
-    ParticlesView *m_view = nullptr;
+    struct DeviceMemory
+    {
+        float *x = nullptr, *y = nullptr, *z = nullptr;
+        float *vx = nullptr, *vy = nullptr, *vz = nullptr;
+        float *m = nullptr, *q = nullptr;
+
+        ~DeviceMemory();
+        DeviceMemory() = default;
+        DeviceMemory(const DeviceMemory &) = delete;
+        DeviceMemory &operator=(const DeviceMemory &) = delete;
+        DeviceMemory(DeviceMemory &&) = delete;
+        DeviceMemory &operator=(DeviceMemory &&) = delete;
+    };
+
+    std::unique_ptr<DeviceMemory> m_device_memory;
 
   public:
     static std::vector<Particle> read_from_file(const std::string &file_name);
@@ -32,7 +43,9 @@ class Particles
     Particles(const std::vector<Particle> &);
     Particles(size_t n, float *x, float *y, float *z, float *vx, float *vy, float *vz, float *m, float *q);
     Particles(const Particles &);
-    Particles(Particles &&);
+    Particles(Particles &&) = default;
+    Particles &operator=(const Particles &);
+    Particles &operator=(Particles &&) = default;
 
     size_t size() const;
 
@@ -41,7 +54,7 @@ class Particles
     void update_and_unview();
 
     ParticlesView cpu_view();
-    std::vector<Particle> as_vector();
+    std::vector<Particle> as_vector() const;
 
-    ~Particles();
+    ~Particles() = default;
 };
--- src/particles.cu
+++ src/particles.cu
@@ -9,9 +9,9 @@
 #include <vector>
 
 Particles::Particles(const std::vector<Particle> &particles)
-    : m_x(new float[particles.size()]), m_y(new float[particles.size()]), m_z(new float[particles.size()]),
-      m_vx(new float[particles.size()]), m_vy(new float[particles.size()]), m_vz(new float[particles.size()]),
-      m_m(new float[particles.size()]), m_q(new float[particles.size()]), m_size(particles.size())
+    : m_x(particles.size()), m_y(particles.size()), m_z(particles.size()),
+      m_vx(particles.size()), m_vy(particles.size()), m_vz(particles.size()),
+      m_m(particles.size()), m_q(particles.size())
 {
     for (size_t i = 0; i < particles.size(); i++)
     {
@@ -27,228 +27,125 @@ Particles::Particles(const std::vector<Particle> &particles)
 }
 
 Particles::Particles(size_t n, float *x, float *y, float *z, float *vx, float *vy, float *vz, float *m, float *q)
-    : m_x(new float[n]), m_y(new float[n]), m_z(new float[n]), m_vx(new float[n]), m_vy(new float[n]),
-      m_vz(new float[n]), m_m(new float[n]), m_q(new float[n]), m_size(n)
+    : m_x(x, x + n), m_y(y, y + n), m_z(z, z + n),
+      m_vx(vx, vx + n), m_vy(vy, vy + n), m_vz(vz, vz + n),
+      m_m(m, m + n), m_q(q, q + n)
 {
-    std::memcpy(m_x, x, n * sizeof(float));
-    std::memcpy(m_y, y, n * sizeof(float));
-    std::memcpy(m_z, z, n * sizeof(float));
-
-    std::memcpy(m_vx, vx, n * sizeof(float));
-    std::memcpy(m_vy, vy, n * sizeof(float));
-    std::memcpy(m_vz, vz, n * sizeof(float));
-
-    std::memcpy(m_m, m, n * sizeof(float));
-    std::memcpy(m_q, q, n * sizeof(float));
 }
 
 Particles::Particles(const Particles &other)
-    : m_x(new float[other.m_size]), m_y(new float[other.m_size]), m_z(new float[other.m_size]),
-      m_vx(new float[other.m_size]), m_vy(new float[other.m_size]), m_vz(new float[other.m_size]),
-      m_m(new float[other.m_size]), m_q(new float[other.m_size]), m_size(other.m_size)
+    : m_x(other.m_x), m_y(other.m_y), m_z(other.m_z),
+      m_vx(other.m_vx), m_vy(other.m_vy), m_vz(other.m_vz),
+      m_m(other.m_m), m_q(other.m_q)
 {
-    const size_t n = other.m_size;
-    std::memcpy(m_x, other.m_x, n * sizeof(float));
-    std::memcpy(m_y, other.m_y, n * sizeof(float));
-    std::memcpy(m_z, other.m_z, n * sizeof(float));
-
-    std::memcpy(m_vx, other.m_vx, n * sizeof(float));
-    std::memcpy(m_vy, other.m_vy, n * sizeof(float));
-    std::memcpy(m_vz, other.m_vz, n * sizeof(float));
-
-    std::memcpy(m_m, other.m_m, n * sizeof(float));
-    std::memcpy(m_q, other.m_q, n * sizeof(float));
 }
 
-Particles::Particles(Particles &&other)
-    : m_x(other.m_x), m_y(other.m_y), m_z(other.m_z), m_vx(other.m_vx), m_vy(other.m_vy), m_vz(other.m_vz),
-      m_m(other.m_m), m_q(other.m_q), m_size(other.m_size)
+Particles &Particles::operator=(const Particles &other)
 {
-    other.m_size = 0;
-    other.m_x = nullptr;
-    other.m_y = nullptr;
-    other.m_z = nullptr;
-    other.m_vx = nullptr;
-    other.m_vy = nullptr;
-    other.m_vz = nullptr;
-    other.m_m = nullptr;
-    other.m_q = nullptr;
+    if (this != &other)
+    {
+        m_x = other.m_x;
+        m_y = other.m_y;
+        m_z = other.m_z;
+        m_vx = other.m_vx;
+        m_vy = other.m_vy;
+        m_vz = other.m_vz;
+        m_m = other.m_m;
+        m_q = other.m_q;
+        m_device_memory.reset();
+    }
+    return *this;
 }
 
 size_t Particles::size() const
 {
-    return m_size;
+    return m_x.size();
 }
 
 ParticlesView Particles::view()
 {
-    if (!m_view)
+    if (!m_device_memory)
     {
-        m_view = new ParticlesView;
+        m_device_memory = std::make_unique<DeviceMemory>();
+        const size_t n = size();
 
-        cudaMalloc(&m_view->x, m_size * sizeof(float));
-        cudaMalloc(&m_view->y, m_size * sizeof(float));
-        cudaMalloc(&m_view->z, m_size * sizeof(float));
+        cudaMalloc(&m_device_memory->x, n * sizeof(float));
+        cudaMalloc(&m_device_memory->y, n * sizeof(float));
+        cudaMalloc(&m_device_memory->z, n * sizeof(float));
 
-        cudaMalloc(&m_view->vx, m_size * sizeof(float));
-        cudaMalloc(&m_view->vy, m_size * sizeof(float));
-        cudaMalloc(&m_view->vz, m_size * sizeof(float));
+        cudaMalloc(&m_device_memory->vx, n * sizeof(float));
+        cudaMalloc(&m_device_memory->vy, n * sizeof(float));
+        cudaMalloc(&m_device_memory->vz, n * sizeof(float));
 
-        cudaMalloc(&m_view->m, m_size * sizeof(float));
-        cudaMalloc(&m_view->q, m_size * sizeof(float));
+        cudaMalloc(&m_device_memory->m, n * sizeof(float));
+        cudaMalloc(&m_device_memory->q, n * sizeof(float));
 
-        cudaMemcpy(m_view->x, m_x, m_size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(m_view->y, m_y, m_size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(m_view->z, m_z, m_size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->x, m_x.data(), n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->y, m_y.data(), n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->z, m_z.data(), n * sizeof(float), cudaMemcpyHostToDevice);
 
-        cudaMemcpy(m_view->vx, m_vx, m_size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(m_view->vy, m_vy, m_size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(m_view->vz, m_vz, m_size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->vx, m_vx.data(), n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->vy, m_vy.data(), n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->vz, m_vz.data(), n * sizeof(float), cudaMemcpyHostToDevice);
 
-        cudaMemcpy(m_view->m, m_m, m_size * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(m_view->q, m_q, m_size * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->m, m_m.data(), n * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(m_device_memory->q, m_q.data(), n * sizeof(float), cudaMemcpyHostToDevice);
     }
 
-    return *m_view;
+    return ParticlesView{m_device_memory->x, m_device_memory->y, m_device_memory->z,
+                         m_device_memory->vx, m_device_memory->vy, m_device_memory->vz,
+                         m_device_memory->m, m_device_memory->q};
 }
 
 void Particles::update_with_device()
 {
-    if (!m_view)
-        throw std::runtime_error("No views found!");
+    if (!m_device_memory)
+        throw std::runtime_error("No device memory allocated!");
 
-    cudaMemcpy(m_x, m_view->x, m_size * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(m_y, m_view->y, m_size * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(m_z, m_view->z, m_size * sizeof(float), cudaMemcpyDeviceToHost);
+    const size_t n = size();
+    cudaMemcpy(m_x.data(), m_device_memory->x, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_y.data(), m_device_memory->y, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_z.data(), m_device_memory->z, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(m_vx, m_view->vx, m_size * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(m_vy, m_view->vy, m_size * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(m_vz, m_view->vz, m_size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_vx.data(), m_device_memory->vx, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_vy.data(), m_device_memory->vy, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_vz.data(), m_device_memory->vz, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(m_m, m_view->m, m_size * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(m_q, m_view->q, m_size * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_m.data(), m_device_memory->m, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(m_q.data(), m_device_memory->q, n * sizeof(float), cudaMemcpyDeviceToHost);
 }
 
 void Particles::update_and_unview()
 {
     update_with_device();
-
-    cudaFree(m_view->x);
-    cudaFree(m_view->y);
-    cudaFree(m_view->z);
-
-    cudaFree(m_view->vx);
-    cudaFree(m_view->vy);
-    cudaFree(m_view->vz);
-
-    cudaFree(m_view->m);
-    cudaFree(m_view->q);
-
-    delete m_view;
-    m_view = nullptr;
+    m_device_memory.reset();
 }
 
-Particles::~Particles()
+Particles::DeviceMemory::~DeviceMemory()
 {
-    if (m_size != 0)
-    {
-        if (m_x != nullptr)
-            delete[] m_x;
-
-        if (m_y != nullptr)
-            delete[] m_y;
-
-        if (m_z != nullptr)
-            delete[] m_z;
-
-        if (m_vx != nullptr)
-            delete[] m_vx;
-
-        if (m_vy != nullptr)
-            delete[] m_vy;
-
-        if (m_vz != nullptr)
-            delete[] m_vz;
-
-        if (m_m != nullptr)
-            delete[] m_m;
-
-        if (m_q != nullptr)
-            delete[] m_q;
-    }
-
-    if (m_view)
-    {
-        cudaFree(m_view->x);
-        cudaFree(m_view->y);
-        cudaFree(m_view->z);
-        cudaFree(m_view->vx);
-        cudaFree(m_view->vy);
-        cudaFree(m_view->vz);
-        cudaFree(m_view->m);
-        cudaFree(m_view->q);
-
-        delete m_view;
-    }
-}
-
-void ParticlesView::cpu_free()
-{
-    if (x)
-    {
-        delete[] x;
-    }
-
-    if (y)
-    {
-        delete[] y;
-    }
-
-    if (z)
-    {
-        delete[] z;
-    }
-
-    if (vx)
-    {
-        delete[] vx;
-    }
-
-    if (vy)
-    {
-        delete[] vy;
-    }
-
-    if (vz)
-    {
-        delete[] vz;
-    }
-
-    if (m)
-    {
-        delete[] m;
-    }
-
-    if (q)
-    {
-        delete[] q;
-    }
-
-    x = y = z = nullptr;
-    vx = vy = vz = nullptr;
-    m = q = nullptr;
+    if (x) cudaFree(x);
+    if (y) cudaFree(y);
+    if (z) cudaFree(z);
+    if (vx) cudaFree(vx);
+    if (vy) cudaFree(vy);
+    if (vz) cudaFree(vz);
+    if (m) cudaFree(m);
+    if (q) cudaFree(q);
 }
 
 ParticlesView Particles::cpu_view()
 {
-    return ParticlesView{m_x, m_y, m_z, m_vx, m_vy, m_vz, m_m, m_q};
+    return ParticlesView{m_x.data(), m_y.data(), m_z.data(),
+                         m_vx.data(), m_vy.data(), m_vz.data(),
+                         m_m.data(), m_q.data()};
 }
 
-std::vector<Particle> Particles::as_vector()
+std::vector<Particle> Particles::as_vector() const
 {
     std::vector<Particle> particles;
+    const size_t n = size();
 
-    for (size_t i = 0; i < m_size; i++)
+    for (size_t i = 0; i < n; i++)
     {
         particles.emplace_back(m_x[i], m_y[i], m_z[i], m_vx[i], m_vy[i], m_vz[i], m_m[i], m_q[i]);
     }
